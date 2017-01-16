cf24d8e0a41140d863b68d31f40bab1136cc833d
Samuel Vijaykumar M <mad.vijay@gmail.com>
2017-01-16 19:41:49 +0530

    Fix the single quote

--- netdata/recipes/publish_nats.rb
+++ netdata/recipes/publish_nats.rb
@@ -9,5 +9,5 @@ end
 cron 'publish to nats' do
   command "/usr/local/bin/nats-pub 
             -s #{node['metrichill']['nats']}
-            'router.register' '{\"host\":\"#{node['metrichill']['register_ip']}\",\"port\":19999,\"uris\":[\"#{node['netdata']['backend']['hostname']}.#{node['metrichill']['domain']}\"],\"tags\":{\"name\":\"local\",\"type\":\"perf\"}}"
+            'router.register' '{\"host\":\"#{node['metrichill']['register_ip']}\",\"port\":19999,\"uris\":[\"#{node['netdata']['backend']['hostname']}.#{node['metrichill']['domain']}\"],\"tags\":{\"name\":\"local\",\"type\":\"perf\"}}'"
 end
