c4d4738d2c6a38794f55eaac8ed2699dff194470
Samuel Vijaykumar M <mad.vijay@gmail.com>
2017-03-09 14:24:48 +0530

    Add relevant tags

--- netdata/recipes/publish_nats.rb
+++ netdata/recipes/publish_nats.rb
@@ -7,5 +7,5 @@ cookbook_file '/usr/local/bin/nats-pub' do
 end
 
 cron 'publish_to_nats' do
-  command "/usr/local/bin/nats-pub -s #{node['metrichill']['nats']} 'router.register' '{\"host\":\"#{node['metrichill']['register_ip']}\",\"port\":19999,\"uris\":[\"#{node['netdata']['backend']['hostname']}-#{node['opsworks']['stack']['name']}.#{node['metrichill']['domain']}\"],\"tags\":{\"name\":\"local\",\"type\":\"perf\"}}'"
+  command "/usr/local/bin/nats-pub -s #{node['metrichill']['nats']} 'router.register' '{\"host\":\"#{node['metrichill']['register_ip']}\",\"port\":19999,\"uris\":[\"#{node['netdata']['backend']['hostname']}-#{node['opsworks']['stack']['name']}.#{node['metrichill']['domain']}\"],\"tags\":{\"name\":\"#{node['netdata']['backend']['hostname']}\",\"stack\":\"#{node['opsworks']['stack']['name']}\",\"layers\":#{node['opsworks']['instance']['layers']} }}'"
 end
