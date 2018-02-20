2e9b123ccffd0c1878c2e6c0e3505c04956076c5
samof76 <samof76@yahoo.com>
2018-02-20 19:14:48 +0530

    Pass variables by hand

--- netdata/recipes/configure.rb
+++ netdata/recipes/configure.rb
@@ -1,7 +1,17 @@
+instance = search("aws_opsworks_instance", "self:true").first
+stack = search("aws_opsworks_stack").first
+
+hostname = instance['hostname']
+stack_name = stack['name']
+
 template '/etc/netdata/netdata.conf' do
   source 'netdata.conf.erb'
   owner 'netdata'
   group 'netdata'
+  variables ({
+    :hostname => hostname,
+    :stack_name => stack_name
+  })
   mode 00660
   notifies :restart, 'service[netdata]', :immediately
 end
