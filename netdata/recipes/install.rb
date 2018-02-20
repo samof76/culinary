f30b0582f67ef8e4fbea8911a24372c059ee08eb
samof76 <samof76@yahoo.com>
2018-02-20 19:55:13 +0530

    Install packahes before anything else

--- netdata/recipes/install.rb
+++ netdata/recipes/install.rb
@@ -5,6 +5,23 @@
 # Copyright 2016, Sambox LLP
 #
 # No rights reserved - Do Redistribute
+# zlib1g-dev uuid-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl
+# autoconf automake curl gcc git libmnl-devel libuuid-devel lm-sensors make MySQL-python nc pkgconfig python python-psycopg2 PyYAML zlib-devel 
+case node['platform_family']
+when 'rhel', 'fedora'
+  ['autoconf', 'automake', 'curl', 'gcc', 'git', 'libmnl-devel', 'libuuid-devel', 'make', 'MySQL-python', 'zlib-devel', 'pkgconfig', 'nc', 'python', 'python-psycopg2', 'PyYAML'].each {
+    |item| package item do
+      action :install
+    end
+  }
+when 'ubuntu', 'debian'
+  ['zlib1g-dev', 'uuid-dev', 'libmnl-dev', 'gcc', 'make', 'git', 'curl', 'jq', 'nodejs', 'autoconf', 'autoconf-archive', 'autogen', 'automake', 'pkg-config'].each {
+    |item| package item do
+      action :install
+    end
+  }
+end
+
 bash 'install netdata' do
   code <<-EOH
   wget https://my-netdata.io/kickstart.sh
