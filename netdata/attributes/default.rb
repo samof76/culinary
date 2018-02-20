21e57f4bafbf6807740b3249f1ab04d07f535400
samof76 <samof76@yahoo.com>
2018-02-20 18:07:56 +0530

    Updating to latest netdata

--- netdata/attributes/default.rb
+++ netdata/attributes/default.rb
@@ -8,15 +8,15 @@
 # buffer on failures = 10
 # timeout ms = 20000
 
-default['netdata']['backend']['enable'] = 'no'
-default['netdata']['backend']['data_source'] = 'average'
-default['netdata']['backend']['type'] = 'openstdb'
-default['netdata']['backend']['destination'] = 'address to opentsdb host_ip'# node['openstdb']['host_ip']
-default['netdata']['backend']['prefix'] = 'netdata'
-default['netdata']['backend']['hostname'] = node['opsworks']['instance']['hostname']
-default['netdata']['backend']['update_every'] = 10
-default['netdata']['backend']['buffer_on_failures'] = 10
-default['netdata']['backend']['timeout_ms'] = 20000
+# default['netdata']['backend']['enable'] = 'no'
+# default['netdata']['backend']['data_source'] = 'average'
+# default['netdata']['backend']['type'] = 'openstdb'
+# default['netdata']['backend']['destination'] = 'address to opentsdb host_ip'# node['openstdb']['host_ip']
+# default['netdata']['backend']['prefix'] = 'netdata'
+# default['netdata']['backend']['hostname'] = node['opsworks']['instance']['hostname']
+# default['netdata']['backend']['update_every'] = 10
+# default['netdata']['backend']['buffer_on_failures'] = 10
+# default['netdata']['backend']['timeout_ms'] = 20000
 
 default['netdata']['registry']['enabled'] = 'no'
 default['netdata']['registry']['registry_to_announce'] = node['opsworks']['instance']['private_dns_name']
--- netdata/recipes/install.rb
+++ netdata/recipes/install.rb
@@ -5,35 +5,7 @@
 # Copyright 2016, Sambox LLP
 #
 # No rights reserved - Do Redistribute
-case node['platform_family']
-when 'rhel', 'fedora'
-  ['autoconf', 'automake', 'curl', 'gcc', 'git', 'libmnl-devel', 'libuuid-devel', 'make', 'zlib-devel', 'pkgconfig', 'nc', 'python', 'python-psycopg2', 'PyYAML'].each {
-    |item| package item do
-      action :install
-    end
-  }
-when 'ubuntu', 'debian'
-  ['zlib1g-dev', 'uuid-dev', 'libmnl-dev', 'gcc', 'make', 'git', 'curl', 'jq', 'nodejs', 'autoconf', 'autoconf-archive', 'autogen', 'automake', 'pkg-config'].each {
-    |item| package item do
-      action :install
-    end
-  }
-end
-
-directory '/tmp/netdata' do
-  recursive true
-  action :delete
-  only_if {::Dir.exists?('/tmp/netdata')}
-end
-
-git '/tmp/netdata' do
-  repository 'https://github.com/firehol/netdata.git'
-  depth 1
-  revision 'master'
-  action :sync
-end
-
-execute "install script" do
-  command './netdata-installer.sh --dont-wait'
-  cwd '/tmp/netdata'
+execute 'install netdata' do
+  command 'bash <(curl -Ss https://my-netdata.io/kickstart.sh) all --dont-wait'
+  action :run
 end
